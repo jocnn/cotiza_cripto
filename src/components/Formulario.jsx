641b172b30edc76dd079082232a1515f47357044
jocnn <ijann_@outlook.com>
2022-05-03 14:52:14 -0300

    definiendo formulario responsive y el submit

---
+++ src/components/Formulario.jsx
@@ -0,0 +1,18 @@
+const Formulario = () => {
+  return (
+    <>
+      <form>
+        <input
+          className="
+          bg-[#9497FF] border-none w-[100%] p-[10px] 
+          text-white font-bold uppercase text-xl 
+            rounded hover:bg-[#7A7DFE] transition-colors 
+            duration-[3000] ease-in cursor-pointer"
+          type="submit" 
+          value="Cotizar" />
+      </form>
+    </>
+  )
+}
+
+export default Formulario
