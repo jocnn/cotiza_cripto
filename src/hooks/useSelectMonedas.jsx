c111a64d987c30b9467ba7c0de8f8547a4c8a95b
jocnn <ijann_@outlook.com>
2022-05-03 19:33:55 -0300

    usando useState en el custom hook

--- src/hooks/useSelectMonedas.jsx
+++ src/hooks/useSelectMonedas.jsx
@@ -1,10 +1,18 @@
+import { useState } from 'react'
+
 const useSelectMonedas = (label, opciones) => {
+
+  const [ state, setState ] = useState()
 
   const SelectMoneda = () => (
     <div className="text-white block font-['Lato'] text-[24px] font-bold my-4 mx-0" >
       <label>
         {label}
-        <select className="text-black text-[18px] p-2 rounded-md w-full text-center">
+        <select
+          className="text-black text-[18px] p-2 rounded-md w-full text-center"
+          value={state}
+          onChange={ e => setState(e.target.value) }
+        >
           <option>--- Seleccione su Moneda ---</option>
           {
             opciones.map( opcion => (
@@ -21,7 +29,7 @@ const useSelectMonedas = (label, opciones) => {
     </div>
   )
   
-  return [ SelectMoneda ]
+  return [ state, SelectMoneda ]
 }
 
 export default useSelectMonedas
