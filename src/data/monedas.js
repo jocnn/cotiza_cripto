aa4bd1f80f51ab74aee51c179b4aac9d2a082a4b
jocnn <ijann_@outlook.com>
2022-05-03 19:20:35 -0300

    datos dummy

---
+++ src/data/monedas.js
@@ -0,0 +1,10 @@
+const divisas = [
+  { id: 'USD', nombre: 'Dolar USA' },
+  { id: 'EUR', nombre: 'Euro' },
+  { id: 'GBP', nombre: 'Libra Esterlina' },
+  { id: 'AR', nombre: 'Peso Argentino' },
+]
+
+export {
+  divisas
+}
