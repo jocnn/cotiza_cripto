a6dde42a01c71bcfb532e459b4919fc0de162050
jocnn <ijann_@outlook.com>
2022-05-04 11:08:28 -0300

    agregando estilos

--- src/components/Resultado.jsx
+++ src/components/Resultado.jsx
@@ -3,12 +3,18 @@ const Resultado = ({ resultado }) => {
   const { PRICE, HIGHDAY, LOWDAY, CHANGEPCT24HOUR, IMAGEURL, LASTUPDATE } = resultado
 
   return (
-    <div>
-      <p>El precio es de: <span>{PRICE}</span></p>
-      <p>Precio más alto del día: <span>{HIGHDAY}</span></p>
-      <p>Precio más bajo del día: <span>{LOWDAY}</span></p>
-      <p>Variación últimas 24 horas: <span>{CHANGEPCT24HOUR}</span></p>
-      <p>Última actualización: <span>{LASTUPDATE}</span></p>
+    <div className="flex items-center gap-3 mt-8 text-white font-['Lato']">
+      <img 
+        className="block w-[120px]"
+        src={`https://cryptocompare.com/${IMAGEURL}`} 
+        alt="imagen cripto" />
+      <div className="">
+        <p className="text-[20px] ">El precio es de: <span className="text-[35px] font-bold">{PRICE}</span></p>
+        <p className="text-[18px] ">Precio más alto del día: <span className="font-bold">{HIGHDAY}</span></p>
+        <p className="text-[18px] ">Precio más bajo del día: <span className="font-bold">{LOWDAY}</span></p>
+        <p className="text-[18px] ">Variación últimas 24 horas: <span className="font-bold">{CHANGEPCT24HOUR}</span></p>
+        <p className="text-[18px] ">Última actualización: <span className="font-bold">{LASTUPDATE}</span></p>
+      </div>
     </div>
   )
 }
